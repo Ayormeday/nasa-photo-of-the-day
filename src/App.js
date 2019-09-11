613e56091f3550bc701c1218bd2e4fbc64a14dca
Ayomide Jones <ayormeday@gmail.com>
2019-09-11 15:57:41 +0100

    imported headerimage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Title from "./header/Title";
-import Date from "./header/Date"
+import Date from "./header/Date";
+
 
 const testApi = "https://lambda-github-api-server.herokuapp.com/";
 const functionalApi = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
@@ -26,6 +27,8 @@ function App() {
     <div className="App">
       <Title title={images.title} />
       <Date date={images.date} />
+      <img src={images.hdurl} />
+
       <p>{images.explanation}</p>
     </div>
   );
