5adcd39f5883488ab5abf2c1322191bc567e65a6
Ayomide Jones <ayormeday@gmail.com>
2019-09-11 13:40:30 +0100

    created the state and efffect hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
+const testApi = "https://lambda-github-api-server.herokuapp.com/"
+const functionalApi = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
 function App() {
+const [images, setimage] = useState();
+
+useEffect(() => {
+  axios.get(testApi)
+    .then(resImages => {debugger
+      console.log(resImages.data)
+    })
+  
+}, []);
+
+
   return (
     <div className="App">
       <p>
